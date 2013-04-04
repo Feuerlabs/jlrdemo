8aafa703f0cdc52ec4673dc7fb3964245f8ceae3
Magnus Feuer <magnus@feuerlabs.com>
2013-04-04 10:40:25 -0700

    Modded device config. Added local start script. Added curl scripts for fan speed.

---
+++ curl_scripts/set-fan-speed.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#  Create new accounts, like the ga account
+#  But other accounts may be create
+. $HOME/.exodmrc
+
+if [ $# != 2 ]
+then
+    echo "Usage: $0 device-id speed(0-10)"
+    exit 255
+fi
+# the password (actually erlang node cookie) must be 100% hidden
+# so this is only for testing!!!!!
+curl -u $USER_AUTH -k -X POST  $URL -d @- << EOF
+{
+    "jsonrpc": "2.0",
+    "method": "jlrdemo:set-fan-speed-request",
+    "id": "1",
+    "params":
+    {
+      "device-id": "$1",
+      "fan-speed": $2
+
+    }
+}
+EOF
